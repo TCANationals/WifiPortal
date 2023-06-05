6a6e4c4f9cefe5fe8c5486cf92df09ac9f89fc9b
Mike Christopher <mchristopher@users.noreply.github.com>
2023-06-05 12:24:43 -0700

    clean up page

--- app.local.js
+++ app.local.js
@@ -1,7 +1,7 @@
 process.env.AWS_REGION = 'us-east-1'
 
 const app = require('./src/app')
-const port = 3002
+const port = 4009
 
 app.listen(port)
 console.info(`listening on http://localhost:${port}`)
